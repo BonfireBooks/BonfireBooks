577605616eba43f717702c46e18fbdde290c0a4b
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2022-10-07 00:10:24 -0400

    Added Routing to the Start Activity

--- app/src/main/java/com/example/bonfirebooks/StartActivity.java
+++ app/src/main/java/com/example/bonfirebooks/StartActivity.java
@@ -2,13 +2,36 @@ package com.example.bonfirebooks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.btn_signin);
+        signup = findViewById(R.id.btn_signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, SignupActivity.class));
+            }
+        });
     }
 }
