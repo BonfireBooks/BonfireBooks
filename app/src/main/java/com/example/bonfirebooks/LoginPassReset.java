f57805bc3dcac786f6715551f67d3136cf165e91
JamesM10101 <jpmiller2000@gmail.com>
2022-10-08 13:58:53 -0400

    Login's password reset activity created

---
+++ app/src/main/java/com/example/bonfirebooks/LoginPassReset.java
@@ -0,0 +1,14 @@
+package com.example.bonfirebooks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPassReset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_pass_reset);
+    }
+}
