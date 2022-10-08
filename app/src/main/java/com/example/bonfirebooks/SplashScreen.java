01faff510d8c22dbfafed747ee4f78e18f1ddb66
JamesM10101 <jpmiller2000@gmail.com>
2022-10-08 14:36:28 -0400

    SplashScreen checks for logged in user

--- app/src/main/java/com/example/bonfirebooks/SplashScreen.java
+++ app/src/main/java/com/example/bonfirebooks/SplashScreen.java
@@ -6,19 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreen extends AppCompatActivity {
+
+    private FirebaseUser currUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        // hold the splash screen for 1s before switching to appropriate activity'
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreen.this, StartActivity.class));
-            }
-        }, 300); // 300 ms
+        currUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        // check if user is currently logged in
+        if(currUser == null) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreen.this, StartActivity.class));
+                }
+            }, 1000);
+        } else {
+            // ToDo -- create intent to the main activity
+        }
+
     }
 }
