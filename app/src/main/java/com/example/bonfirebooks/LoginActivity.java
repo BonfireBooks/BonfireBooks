dd07b76c891fa357cdb0dc83d16011bb5aabec72
JamesM10101 <jpmiller2000@gmail.com>
2022-10-08 14:15:18 -0400

    LoginActivity routing added

--- app/src/main/java/com/example/bonfirebooks/LoginActivity.java
+++ app/src/main/java/com/example/bonfirebooks/LoginActivity.java
@@ -2,13 +2,47 @@ package com.example.bonfirebooks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView forgotPass;
+    private Button btn_signUp;
+    private Button btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        forgotPass = findViewById(R.id.txtV_forgot_password);
+        btn_signUp = findViewById(R.id.btn_signup);
+        btn_back = findViewById(R.id.btn_back);
+
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, LoginPassReset.class));
+            }
+        });
+
+        btn_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+                finish();
+            }
+        });
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
