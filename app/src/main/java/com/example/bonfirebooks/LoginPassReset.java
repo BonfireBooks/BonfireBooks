9b79e7d4ddfd181bdc3a15de15cb429dd2feaebe
JamesM10101 <jpmiller2000@gmail.com>
2022-10-08 14:19:28 -0400

    LoginPassReset routing added for back arrow

--- app/src/main/java/com/example/bonfirebooks/LoginPassReset.java
+++ app/src/main/java/com/example/bonfirebooks/LoginPassReset.java
@@ -3,12 +3,25 @@ package com.example.bonfirebooks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPassReset extends AppCompatActivity {
+
+    private Button btn_Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_pass_reset);
+
+        btn_Back = findViewById(R.id.btn_back);
+
+        btn_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
