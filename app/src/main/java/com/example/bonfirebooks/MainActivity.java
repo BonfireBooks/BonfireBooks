17ed7ee1f282038e5a7ca3e06d7396520862d116
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2022-10-16 22:48:47 -0400

    Added routing for navigation bar

--- app/src/main/java/com/example/bonfirebooks/MainActivity.java
+++ app/src/main/java/com/example/bonfirebooks/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.bonfirebooks;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import org.checkerframework.common.subtyping.qual.Bottom;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigationView = findViewById(R.id.bottomNavView);
+        navigationView.setOnItemSelectedListener(this);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new HomeFragment()).commit();
         }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new HomeFragment()).commit();
+                break;
+            case R.id.nav_wish_list:
+                //  Todo -- add navigation to a wishlist fragment
+                break;
+            case R.id.nav_chats:
+                //  Todo -- add navigation to a chats fragment
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new AccountFragment()).commit();
+                break;
+        }
+
+        return true;
     }
 }
