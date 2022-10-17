e4fa234b82b1255116e9257ee797cac147dd4ac2
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2022-10-16 22:32:57 -0400

    Account Fragment sign out functionality added

--- app/src/main/java/com/example/bonfirebooks/AccountFragment.java
+++ app/src/main/java/com/example/bonfirebooks/AccountFragment.java
@@ -1,12 +1,18 @@
 package com.example.bonfirebooks;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +66,21 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // user sign out
+        Button logout = getView().findViewById(R.id.btn_logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), StartActivity.class));
+                getActivity().finish();
+            }
+        });
     }
 }
