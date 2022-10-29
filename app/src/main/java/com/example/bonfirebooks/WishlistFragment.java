fe171e86ef0ab05c143ecb259eea98eaa657ec9d
JamesM10101 <jpmiller2000@gmail.com>
2022-10-29 19:06:43 -0400

    Wishlist Fragment added scroll view

--- app/src/main/java/com/example/bonfirebooks/WishlistFragment.java
+++ app/src/main/java/com/example/bonfirebooks/WishlistFragment.java
@@ -2,6 +2,8 @@ package com.example.bonfirebooks;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -60,5 +62,14 @@ public class WishlistFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_wishlist, container, false);
+    }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
