e78d0495d61bcd48ee330c5ca59fbeba05d5f004
JamesM10101 <jpmiller2000@gmail.com>
2022-10-29 19:38:47 -0400

    Wishlist Fragment now shows wishlisted books

--- app/src/main/java/com/example/bonfirebooks/WishlistFragment.java
+++ app/src/main/java/com/example/bonfirebooks/WishlistFragment.java
@@ -4,11 +4,27 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -64,12 +80,80 @@ public class WishlistFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_wishlist, container, false);
     }
 
+    // layout items
+    ConstraintLayout layout_wishlist_empty;
+    ScrollView scrollV_wishlist;
+    LinearLayout linlayout_wishlist;
 
+    // Firebase
+    FirebaseFirestore firestore;
+    FirebaseUser user;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        layout_wishlist_empty = view.findViewById(R.id.layout_wishlist_empty);
+        scrollV_wishlist = view.findViewById(R.id.scrollV_wishlist);
+        linlayout_wishlist = view.findViewById(R.id.linlayout_wishlist);
 
+        // firestore
+        firestore = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        populateScrollViewWithFirebase(linlayout_wishlist);
+
+    }
+
+    private void populateScrollViewWithFirebase(LinearLayout linearLayout) {
+        firestore.collection("users").document(user.getUid()).collection("wishlist").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    Log.d("getUserWishlist", "Successful");
+                    QuerySnapshot taskResults = task.getResult();
+                    if(taskResults.size() < 1) {
+                        layout_wishlist_empty.setVisibility(View.VISIBLE);
+
+                        int i = 0;
+                        // add all the docs into the vertical scroll
+                        for (DocumentSnapshot taskDoc : task.getResult().getDocuments()) {
+
+                            View bookView = getLayoutInflater().inflate(R.layout.wishlist_book_item, null);
+                            bookView.setPadding(30, 0, 0, 0);
+
+                            // new book view details
+                            ImageView book_image = bookView.findViewById(R.id.imgV_coverImage);
+                            TextView book_title = bookView.findViewById(R.id.txtV_book_title);
+                            TextView book_price = bookView.findViewById(R.id.txtV_book_price);
+
+                            // set book views image
+                            Picasso.get().load(taskDoc.getString("coverImgUrl")).into(book_image, new Callback() {
+                                @Override
+                                public void onSuccess() {
+                                    book_image.setBackground(null);
+                                }
+
+                                @Override
+                                public void onError(Exception e) {
+                                    // do nothing -- keep image not found background
+                                }
+                            });
+
+                            // set other book view details
+                            book_title.setText(taskDoc.getString("title"));
+                            book_price.setText(taskDoc.getDouble("price").toString());
+
+                            // add the book to the layout
+                            linearLayout.addView(bookView, i);
+
+                            i++;
+                        }
+                    }
+                } else {
+                    Log.d("getUserWishlist", "Failed");
+                }
+            }
+        });
     }
 }
