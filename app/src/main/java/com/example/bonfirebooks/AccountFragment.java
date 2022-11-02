7cac759f130071360789643edd84fd09936cb3e8
JamesM10101 <jpmiller2000@gmail.com>
2022-11-02 14:22:26 -0400

    AccountFragment Updated
    Removed Chat and Wishlist buttons in drawer and added click listeners for all drawer text views

--- app/src/main/java/com/example/bonfirebooks/AccountFragment.java
+++ app/src/main/java/com/example/bonfirebooks/AccountFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,21 +16,14 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AccountFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class AccountFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -39,16 +33,12 @@ public class AccountFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AccountFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
+    public static AccountFragment newInstance() {
         AccountFragment fragment = new AccountFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,8 +47,6 @@ public class AccountFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -69,16 +57,34 @@ public class AccountFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
 
+    User user;
+
     Button logout;
     TextView txtV_upload_book;
+    TextView txtV_view_books;
+    TextView txtV_edit_profile;
+    TextView txtV_order_history;
+    TextView txtV_notification;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        logout = getView().findViewById(R.id.btn_logout);
-        txtV_upload_book = getView().findViewById(R.id.txtV_upload_book);
+        user = ((MainActivity)getActivity()).getUser();
 
+        Log.d("account", user.toString());
+
+        logout = view.findViewById(R.id.btn_logout);
+        txtV_edit_profile = view.findViewById(R.id.txtV_edit_profile);
+        txtV_view_books = view.findViewById(R.id.txtV_view_books);
+        txtV_upload_book = view.findViewById(R.id.txtV_upload_book);
+        txtV_order_history = view.findViewById(R.id.txtV_order_history);
+        txtV_notification = view.findViewById(R.id.txtV_notification);
+
+        setViewListeners();
+    }
+
+    private void setViewListeners() {
         // user sign out
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,11 +95,40 @@ public class AccountFragment extends Fragment {
             }
         });
 
+        txtV_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // move fragments here
+            }
+        });
+
+        txtV_view_books.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // move fragments here
+            }
+        });
+
         txtV_upload_book.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getParentFragmentManager().beginTransaction().replace(R.id.frame_container, new UploadBookSearchFragment()).commit();
             }
         });
+
+        txtV_order_history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // move fragments here
+            }
+        });
+
+        txtV_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // move fragments here
+            }
+        });
+
     }
 }
