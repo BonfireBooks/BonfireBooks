a00e35598b037ecf951e78f61bf0a70691bd0e9d
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2022-11-07 00:26:07 -0500

    AllBooksFragment now loads books into grid

--- app/src/main/java/com/example/bonfirebooks/AllBooksFragment.java
+++ app/src/main/java/com/example/bonfirebooks/AllBooksFragment.java
@@ -6,9 +6,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
@@ -52,10 +59,59 @@ public class AllBooksFragment extends Fragment {
 
     HashMap<Integer, Book> booksByTitle = new HashMap<>();
 
+    GridLayout gridL_all_books;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         booksByTitle = ((MainActivity)getActivity()).getBooksByTitle();
+
+        gridL_all_books = view.findViewById(R.id.gridL_all_books);
+
+        for (int i = 0; i < booksByTitle.size(); i++) {
+            Book currBook = booksByTitle.get(i);
+
+            View bookView = getLayoutInflater().inflate(R.layout.home_book_item, null);
+
+            // add some spacing between the grid items
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.setMargins(10, 10, 10, 10);
+
+            bookView.setLayoutParams(params);
+
+            // new book view details
+            ImageView book_image = bookView.findViewById(R.id.imgV_coverImage);
+            TextView book_title = bookView.findViewById(R.id.txtV_book_title);
+            TextView book_price = bookView.findViewById(R.id.txtV_book_price);
+
+            // set book views image
+            Picasso.get().load(currBook.getCoverImgUrl()).into(book_image, new Callback() {
+                @Override
+                public void onSuccess() {
+                    book_image.setBackground(null);
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    // do nothing -- keep image not found background
+                }
+            });
+
+            // set other book view details
+            book_title.setText(currBook.getTitle());
+            book_price.setText("$ " + currBook.getPrice());
+
+            bookView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("book", currBook.toString());
+                    getParentFragmentManager().beginTransaction().replace(R.id.frame_container, new BookDetailsFragment(currBook)).commit();
+                }
+            });
+
+            // add the book to the layout
+            gridL_all_books.addView(bookView);
+        }
     }
 }
