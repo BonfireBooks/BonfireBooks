faed8e30742287bce4da341c2169169431e1d3eb
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2022-11-10 13:37:38 -0500

    BookOfferDetailsFragment added functionality for wishlist button

--- app/src/main/java/com/example/bonfirebooks/BookOfferDetailsFragment.java
+++ app/src/main/java/com/example/bonfirebooks/BookOfferDetailsFragment.java
@@ -25,6 +25,9 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
@@ -80,6 +83,12 @@ public class BookOfferDetailsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_book_offer_details, container, false);
     }
 
+    User user;
+
+    FirebaseFirestore firestore;
+
+    HashMap<String, WishlistBook> userWishlsit;
+
     HorizontalScrollView horizScrollV_images;
     LinearLayout linlayout_image_scroll;
 
@@ -89,12 +98,19 @@ public class BookOfferDetailsFragment extends Fragment {
     TextView txtV_condition_edit;
 
     Button btn_back;
+    Button btn_wishlist;
     Button btn_email_seller;
     Button btn_message_seller;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        user = ((MainActivity)getActivity()).getUser();
+
+        firestore = FirebaseFirestore.getInstance();
+
+        userWishlsit = user.getWishlist();
 
         horizScrollV_images = view.findViewById(R.id.horizScrollV_images);
         linlayout_image_scroll = view.findViewById(R.id.linlayout_image_scroll);
@@ -105,8 +121,9 @@ public class BookOfferDetailsFragment extends Fragment {
         btn_back = view.findViewById(R.id.btn_back);
         btn_email_seller = view.findViewById(R.id.btn_email_seller);
         btn_message_seller = view.findViewById(R.id.btn_message_seller);
+        btn_wishlist = view.findViewById(R.id.btn_wishlist);
 
-
+        // set the images for the book
         if (userBook.getPathsToImages() == null) {
             addImageToScroll(book.getCoverImgUrl(), 0);
         } else {
@@ -116,11 +133,14 @@ public class BookOfferDetailsFragment extends Fragment {
             }
         }
 
+        // set the details of the book
         txtV_book_title.setText(book.getTitle());
         txtV_book_price.setText("$ " + userBook.getPrice());
         txtV_seller_edit.setText(userBook.getUserName());
         txtV_condition_edit.setText(userBook.getCondition());
 
+
+        // back button click
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -128,15 +148,69 @@ public class BookOfferDetailsFragment extends Fragment {
             }
         });
 
+        final boolean[] inWishlist = {false};
+
+        // set the background of the wishlist button
+        if(userWishlsit != null) {
+            for (int i = 0; i < userWishlsit.size(); i++) {
+                if(userBook.getBookId().equals(userWishlsit.get(String.valueOf(i)).getBookId())) {
+                    btn_wishlist.setBackground(getResources().getDrawable(R.drawable.ic_heart_filled));
+                    inWishlist[0] = true;
+                    break;
+                }
+            }
+        }
+
+        // wishlist toggle button click
+        btn_wishlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // the book is wishlisted
+                if(inWishlist[0]) {
+                    // unwishlist
+                    firestore.collection("users").document(user.getUid()).collection("wishlist").document(userBook.getBookId()).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Log.d("removeFromWishlist", "Successful");
+                                inWishlist[0] = false;
+                                btn_wishlist.setBackground(getResources().getDrawable(R.drawable.ic_heart_empty));
+                            } else {
+                                Log.d("removeFromWishlist", "Failed");
+                            }
+                        }
+                    });
+                } else {
+                    // add to wishlist
+                    WishlistBook wishBook = new WishlistBook(userBook.getBookId(), book.getTitle(), userBook.getCondition(), book.getCoverImgUrl(), userBook.getPrice());
+                    firestore.collection("users").document(user.getUid()).collection("wishlist").document(userBook.getBookId()).set(wishBook).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Log.d("addToWishlist", "Successful");
+                                inWishlist[0] = true;
+                                btn_wishlist.setBackground(getResources().getDrawable(R.drawable.ic_heart_filled));
+                            } else {
+                                Log.d("addToWishlist", "Failed");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        // email seller button click
         btn_email_seller.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                // create the intent
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("message/rfc822");
                 intent.putExtra(Intent.EXTRA_EMAIL, new String[] { userBook.getEmail() });
                 intent.putExtra(Intent.EXTRA_SUBJECT, "Regarding Your " + book.getTitle() + " Posting On Bonfire");
 
+                // set up a chooser for the available e-mail apps
                 Intent.createChooser(intent, "Choose an e-mail app");
                 if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
                     startActivity(intent);
