daf122171b9a6c841e4b25719e5e810b1c4fefa2
JamesM10101 <jpmiller2000@gmail.com>
2022-11-12 19:30:02 -0500

    AccountEdit Fragment Created

---
+++ app/src/main/java/com/example/bonfirebooks/AccountEditFragment.java
@@ -0,0 +1,67 @@
+package com.example.bonfirebooks;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AccountEditFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AccountEditFragment extends Fragment {
+
+    public AccountEditFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment EditAccountFragment.
+     */
+    public static AccountEditFragment newInstance() {
+        AccountEditFragment fragment = new AccountEditFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_edit, container, false);
+    }
+
+    FirebaseUser firebaseUser;
+
+    User user;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        user = ((MainActivity)getActivity()).getUser();
+
+    }
+}
