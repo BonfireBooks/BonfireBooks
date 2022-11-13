2c8d0127c94fb0a4e3ab88740c5e737864381cfa
JamesM10101 <jpmiller2000@gmail.com>
2022-11-13 13:16:47 -0500

    UserBooksDetailsFragment Created

---
+++ app/src/main/java/com/example/bonfirebooks/UserBooksDetailsFragment.java
@@ -0,0 +1,55 @@
+package com.example.bonfirebooks;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UserBooksDetailsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UserBooksDetailsFragment extends Fragment {
+
+    public UserBooksDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment UserBooksDetailsFragment.
+     */
+    public static UserBooksDetailsFragment newInstance() {
+        UserBooksDetailsFragment fragment = new UserBooksDetailsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_books_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
