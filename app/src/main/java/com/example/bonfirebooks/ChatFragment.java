8f0ea3810297bdbc0eaf9e07bf2923537a80a3cc
JamesM10101 <98126631+JamesM10101@users.noreply.github.com>
2022-11-18 23:13:41 -0500

    ChatFragment Created

---
+++ app/src/main/java/com/example/bonfirebooks/ChatFragment.java
@@ -0,0 +1,62 @@
+package com.example.bonfirebooks;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChatFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChatFragment extends Fragment {
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+    UserProfileChat userProfileChat;
+
+    public ChatFragment(UserProfileChat userProfileChat) {
+        this.userProfileChat = userProfileChat;
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment ChatFragment.
+     */
+    public static ChatFragment newInstance() {
+        ChatFragment fragment = new ChatFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
